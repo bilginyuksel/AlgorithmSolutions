3364d0ca1b06fd7c2fafaf5fe3bafdc6fe51294d
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-06-13 20:40:22 +0300

    feat(leetcode): 287.find duplicate number added

---
+++ leetcode/findDuplicateNumber.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+        
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+    
+solution = Solution()
+duplicated = solution.findDuplicate([1,3,4,2,2])
+print(duplicated)
